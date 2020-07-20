a8a97843f4826d950bc01b8aecdd2caec07b2f7d
Sam Wilkins <samwilkins333@gmail.com>
2020-07-20 02:13:21 -0700

    added regex trie word validation

--- vocabulary/trie_factory.c
+++ vocabulary/trie_factory.c
@@ -1,4 +1,5 @@
 #include "trie_factory.h"
+#include <regex.h>
 
 inline trie_t *construct_trie_from(const char *relative_filepath) {
     trie_t *trie = trie_initialize();
@@ -10,10 +11,17 @@ inline trie_t *construct_trie_from(const char *relative_filepath) {
         perror("Failed to read dictionary.");
         return NULL;
     }
+    regex_t verifier;
+    if (regcomp(&verifier, "[a-z]+", REG_EXTENDED)) {
+        return NULL;
+    }
     size_t len;
     char *word;
     while (getline(&word, &len, dictionary) != -1) {
         word[strcspn(word, "\n")] = 0;
+        if (regexec(&verifier, word, 0, NULL, 0)) {
+            exit(1);
+        }
         trie_add_word(trie, word);
     }
     fclose(dictionary);
