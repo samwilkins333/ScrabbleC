267cd8f2cf9f1b3977f151622e4f6830dbd78198
Sam Wilkins <samwilkins333@gmail.com>
2020-07-20 02:16:41 -0700

    added basic dictionary contents validation

--- vocabulary/trie_factory.c
+++ vocabulary/trie_factory.c
@@ -1,5 +1,4 @@
 #include "trie_factory.h"
-#include <regex.h>
 
 inline trie_t *construct_trie_from(const char *relative_filepath) {
     trie_t *trie = trie_initialize();
@@ -11,16 +10,15 @@ inline trie_t *construct_trie_from(const char *relative_filepath) {
         perror("Failed to read dictionary.");
         return NULL;
     }
-    regex_t verifier;
-    if (regcomp(&verifier, "[a-z]+", REG_EXTENDED)) {
-        return NULL;
-    }
+
     size_t len;
     char *word;
     while (getline(&word, &len, dictionary) != -1) {
         word[strcspn(word, "\n")] = 0;
-        if (regexec(&verifier, word, 0, NULL, 0)) {
-            exit(1);
+        for (int i = 0; i < strlen(word); ++i) {
+            if (word[i] > 122 || word[i] < 97) {
+                exit(1);
+            }
         }
         trie_add_word(trie, word);
     }
